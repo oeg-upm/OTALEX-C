7123f2758deb01a2c9c3915552956e39139a089e
prodrig-gsl <prodrig@geoslab.com>
2015-10-13 15:36:31 +0200

    Add link CSW how resource to all metadata from CSW harvest

--- ckanext-geoslab_spatial/ckanext/geoslab_spatial/harvesters/csw_geoslab.py
+++ ckanext-geoslab_spatial/ckanext/geoslab_spatial/harvesters/csw_geoslab.py
@@ -6,11 +6,10 @@ Created on 28/9/2015
 
 import logging
 import pprint
-import json
 
 import ckan.plugins as p
 from ckanext.spatial.interfaces import ISpatialHarvester
-
+from ckanext.spatial.harvesters.base import guess_resource_format
 from ckanext.spatial.harvesters import CSWHarvester
 
 #class CswHarvesterGeoslab(p.SingletonPlugin):
@@ -30,14 +29,12 @@ class CswHarvesterGeoslab(CSWHarvester):
         # Configuramos el log
         log = logging.getLogger(__name__ + '.geoslab')
         log.debug('CswHarvesterGeoslab: get_package_dict.')
-        
-        # Mostramos los datos que tenemos
         log.debug('CswHarvesterGeoslab: iso_values = \n' + pprint.pformat(iso_values, indent=4) + '\n')
         
         # Ejecutamos el proceso padre
         data_dict = super(CSWHarvester, self).get_package_dict(iso_values, harvest_object)
     
-        log.debug('CswHarvesterGeoslab: responsible-party = \n' + pprint.pformat(data_dict, indent=4) + '\n')
+        log.debug('CswHarvesterGeoslab: data_dict = \n' + pprint.pformat(data_dict, indent=4) + '\n')
     
         # TAGS - Cortamos los tags con puntos
         
@@ -57,5 +54,20 @@ class CswHarvesterGeoslab(CSWHarvester):
                 tags.append({'name': tag})
         # Machacamos los que había
         data_dict['tags'] = tags
+        
+        # RECURSOS
+        
+        # Creamos el recurso al metadato en el csw
+        resource = {}
+        # Ejemplo: http://www.ideotalex.eu/geonetwork/srv/csw?request=GetRecordById&service=CSW&version=2.0.2&outputSchema=http://www.isotc211.org/2005/gmd&id=908b40e5-588b-4d70-aa61-63c98aed7d88
+        resource['url'] = harvest_object.source.url + '?request=GetRecordById&service=CSW&version=2.0.2&outputSchema=http://www.isotc211.org/2005/gmd&id=' + harvest_object.guid
+        resource['format'] = guess_resource_format(resource.get('url'))
+        resource['protocol'] = ''
+        resource['name'] = 'Metadato ISO19115'
+        resource['description'] = 'Enlace al metadato en el csw'
+        resource['resource_locator_protocol]'] = resource.get('protocol')
+        resource['resource_locator_function'] = ''
+        # Lo añadimos al recurso
+        data_dict['resources'].append(resource)
         
         return data_dict

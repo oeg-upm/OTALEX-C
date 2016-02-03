919eac9bfe872296070643875ae649e9b9454646
prodrig-gsl <prodrig@geoslab.com>
2016-02-03 15:56:30 +0100

    Add translate for the ckanext-geoslab_harvest

--- ckanext-geoslab_harvest/setup.py
+++ ckanext-geoslab_harvest/setup.py
@@ -73,6 +73,14 @@ setup(
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     data_files=[],
 
+    message_extractors={
+        'ckanext': [
+            ('**.py', 'python', None),
+            ('**.js', 'javascript', None),
+            ('**/templates/**.html', 'ckan', None),
+        ],
+    },
+
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
